c2047ac13f6efa1e5ac41f854d0b9f7e03b06166
Mark Witmer <mark.d.witmer@gmail.com>
2012-09-01 16:14:56 -0400

    Ignore extensions.scm

--- modules/stay-alive/extensions.scm
+++
@@ -1,7 +0,0 @@
-(define-module (stay-alive extensions)
-               #:export (dijkstra
-                          find-paths
-                          see-level))
-
-(load-extension "libguile_dijkstra" "init_dijkstra")
-(load-extension "libguile_fov" "init_fov")
