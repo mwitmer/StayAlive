741f094d8f1361480e46642e5bb367ed5250c86b
Mark Witmer <mark.d.witmer@gmail.com>
2013-01-13 16:50:31 -0500

    Started the arduous process of moving StayAlive over to the new Shelf interface... better sooner than later!

--- modules/stay-alive/item.scm
+++ modules/stay-alive/item.scm
@@ -14,7 +14,7 @@
 
 (define-objects Item
   ((initialize! (method ()
-		  (set! (this 'timers) '())))
+		  (set! ($ this 'timers) '())))
    (quantity 1)
    (lit? #f)
    (weapon? #f)
@@ -22,43 +22,43 @@
    (hit-agent 
     (method (level source target distance)
       (message-player level 
-		      (target 'location) 
+		      ($ target 'location) 
 		      (format #f "~a ~a ~a" 
-			      (this 'describe-definite) 
+			      ($ this 'describe-definite) 
 			      (conjugate-verb 'to-hit #f) 
-			      (target 'describe-definite)))))
+			      ($ target 'describe-definite)))))
    (describe-inventory 
     (method ()
-      (format #f "~a - ~a" (this 'letter) (this 'describe-short))))
+      (format #f "~a - ~a" ($ this 'letter) ($ this 'describe-short))))
    (describe-definite
     (method ()
-      (let ((quantity (this 'quantity))
-	    (name (this 'name)))
+      (let ((quantity ($ this 'quantity))
+	    (name ($ this 'name)))
 	(format #f "the ~a" (if (> quantity 1) 
 				(if (list? name) (cadr name) (string-append name "s"))
 				(if (list? name) (car name) name))))))
    (describe-short 
     (method ()
-      (let* ((quantity (this 'quantity))
-	     (name (this 'name))
+      (let* ((quantity ($ this 'quantity))
+	     (name ($ this 'name))
 	     (base (if (= quantity 1)
 		       (format #f "~a ~a" (indefinite-article-for name) (if (list? name) (car name) name))
 		       (format #f "~a ~a" quantity (if (list? name) (cadr name) (string-append name "s"))))))
-	(fold (lambda (prop prev) (if (this (car prop) #:def #f) (format #f "~a (~a)" prev (cdr prop)) prev)) 
+	(fold (lambda (prop prev) (if ($ this (car prop) #:def #f) (format #f "~a (~a)" prev (cdr prop)) prev)) 
 	      base 
 	      '((wielded? . "wielded")
 		(worn? . "worn")
 		(lit? . "lit"))))))
    (wield-requires (h (quantity 1)
-		      (pred (method (part) (eq? (part 'name) 'hand)))))
+		      (pred (method (part) (eq? ($ part 'name) 'hand)))))
    (put-on 
     (method (level agent)				
       (and-let* 
 	  ((eligible-parts
-	    (let ((wear-matches ((agent 'body) 'find 
-				 (pred-and (this #:op 'get '(wear-requires pred))
-					   (pred-not (applicator 'wearing? #:def #f))))))
-	      (if (this '(wear-requires also-wield?) #:def #f)
+	    (let ((wear-matches ($ ($ agent 'body) 'find 
+				   (pred-and ($ this #:op 'get '(wear-requires pred))
+					     (pred-not (applicator 'wearing? #:def #f))))))
+	      (if ($ this '(wear-requires also-wield?) #:def #f)
 		  (filter (lambda (part)
 			    (not (fold (lambda (a b) (or a b)) #f 
 				       (map (lambda (part) (part 'wielding? #:def #f)) 
@@ -79,66 +79,66 @@
 			       (filter (lambda (part) 
 					 (eq? (part 'name) (this '(wear-requires also-wield?))))             
 				       (part 'all-parts)))) parts))
-	      (set! (this 'worn?) #t)						      
-	      (set! (this 'worn-on) (map (applicator #:op 'reference) parts))
-	      (message-player level (agent 'location) (sentence-agent-verb-item agent 'to-put-on this))
+	      (set! ($ this 'worn?) #t)						      
+	      (set! ($ this 'worn-on) (map object-reference parts))
+	      (message-player level ($ agent 'location) (sentence-agent-verb-item agent 'to-put-on this))
 	      #t)
 	    #f))))
    (take-off 
     (method (level agent)
-      (and-let* (((this 'worn? #:def #f))
-		 (parts (this 'worn-on #:def #f)))
+      (and-let* ((($ this 'worn? #:def #f))
+		 (parts ($ this 'worn-on #:def #f)))
 	(for-each (obj-setter 'wearing? #f) parts)
-	(if (this '(wear-requires also-wield?) #:def #f)
+	(if ($ this '(wear-requires also-wield?) #:def #f)
 	    (for-each 
 	     (lambda (part)
 	       (map
 		(obj-setter 'wielding? #f)
-		(filter (lambda (part) (eq? (part 'name) (this '(wear-requires also-wield?)))) 
-			(part 'all-parts)))) parts))
-	(message-player level (agent 'location) (sentence-agent-verb-item agent 'to-take-off this))
-	(set! (this 'worn-on) #f)
-	(set! (this 'worn?) #f)
+		(filter (lambda (part) (eq? ($ part 'name) ($ this '(wear-requires also-wield?)))) 
+			($ part 'all-parts)))) parts))
+	(message-player level ($ agent 'location) (sentence-agent-verb-item agent 'to-take-off this))
+	(set! ($ this 'worn-on) #f)
+	(set! ($ this 'worn?) #f)
 	#t)))
    (unwield 
     (method (level agent)
-      (let ((parts (this 'wielded-with #:def #f)))
+      (let ((parts ($ this 'wielded-with #:def #f)))
 	(if parts (for-each (obj-setter 'wielding? #f) parts)))
-      (if (this 'wielded? #:def #f) 
+      (if ($ this 'wielded? #:def #f) 
 	  (begin
-	    (message-player level (agent 'location) (sentence-agent-verb-item agent 'to-unwield this))
-	    (set! (this 'wielded-with) #f)
-	    (set! (this 'wielded?) #f)
+	    (message-player level ($ agent 'location) (sentence-agent-verb-item agent 'to-unwield this))
+	    (set! ($ this 'wielded-with) #f)
+	    (set! ($ this 'wielded?) #f)
 	    #t)
 	  #f)))
    (wield 
     (method (level agent)
       (and-let* 
 	  ((eligible-parts 
-	    ((agent 'body) 'find (pred-and (this #:op 'get '(wield-requires pred)) 
-					   (pred-not (applicator 'wielding? #:def #f)))))
+	    ($ ($ agent 'body) 'find (pred-and (object-get this '(wield-requires pred)) 
+					       (pred-not (applicator 'wielding? #:def #f)))))
 	   (parts 
-	    (select-part-from-list eligible-parts "with" (this #:op 'get '(wield-requires quantity) #:def 1))))
+	    (select-part-from-list eligible-parts "with" (object-get this '(wield-requires quantity) #:def 1))))
 	(if parts
-	    (begin (for-each (lambda (part) (set! (part 'wielding?) #t)) parts)
-		   (set! (this 'wielded?) #t)
-		   (set! (this 'wielded-with) (map (applicator #:op 'reference) parts))
-		   (message-player level (agent 'location) (sentence-agent-verb-item agent 'to-wield this))
+	    (begin (for-each (lambda (part) (set! ($ part 'wielding?) #t)) parts)
+		   (set! ($ this 'wielded?) #t)
+		   (set! ($ this 'wielded-with) (map (applicator #:op 'reference) parts))
+		   (message-player level ($ agent 'location) (sentence-agent-verb-item agent 'to-wield this))
 		   #t)
 	    #f)))))
   ((Lightsource 
     ((lightable? #t)
      (use (method (level agent) 	
-	    (if (this 'lightable?)
+	    (if ($ this 'lightable?)
 		(begin (message-player
-			level (agent 'location) 
-			(sentence-agent-verb-item agent (if (this 'lit?)'to-extinguish 'to-light) this))
-		       (set! (this 'lit?) (not (this 'lit?)))
+			level ($ agent 'location) 
+			(sentence-agent-verb-item agent (if ($ this 'lit?) 'to-extinguish 'to-light) this))
+		       (set! ($ this 'lit?) (not ($ this 'lit?)))
 		       #t)
 		(if (agent 'player?) 
 		    (begin (refresh-level-view)
 			   (message (format #f "~a is burnt out." 
-					    (capitalize (this 'describe-definite))))) #f))))
+					    (capitalize ($ this 'describe-definite))))) #f))))
      (symbol 'tool))
     ((Floodlight
       ((name "floodlight")
@@ -147,7 +147,7 @@
      (Torch  
       ((initialize! 
 	(method ()
-	  (set! (this 'timers) 
+	  (set! ($ this 'timers) 
 		(list (instance LightSourceTimer #:args `(,this 2000))))))
        (name '("torch" "torches"))
        (weight (pounds 4))
